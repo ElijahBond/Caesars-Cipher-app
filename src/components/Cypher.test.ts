23a0e6d408ea27fe2e7a36d86bdadffc8366c252
Elijah Bond <bondarefil@gmail.com>
2023-04-22 00:24:30 +0300

    disable incorrect  test

--- src/components/Cypher.test.ts
+++ src/components/Cypher.test.ts
@@ -1,16 +1,14 @@
-import { render, fireEvent, screen } from '@testing-library/react';
-import { Cypher } from './Cypher.component';
-import * as React from 'react';
-import '@testing-library/jest-dom'
+// import { render, fireEvent, screen } from '@testing-library/react';
+// import { Cypher } from './Cypher.component';
+// import * as React from 'react';
+// import '@testing-library/jest-dom'
 
 
-test('test with key > 0 && key < 26', () => {
+// test('test with key > 0 && key < 26', () => {
 
-render(<Cypher />);
+// render(<Cypher />);
 
-const cypheredWord = screen.getByTestId("cypheredWord");
-const resultButton = screen.getByTestId("resultButton");
+// const cypheredWord = screen.getByTestId("cypheredWord");
+// const resultButton = screen.getByTestId("resultButton");
 
-
-
-});
+// });
